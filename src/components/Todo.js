8ae8f71bcf9a6f7c80991496d5457a863fd0acea
Limo Vincent <48591371+LimoVincent@users.noreply.github.com>
2021-01-22 12:53:11 +0200

    Added filterButton and Form compopnents insdied component directory

---
+++ src/components/Todo.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Todo({ todo }) {
+  const { name, completed, id } = todo
+  return (
+    <li className='todo stack-small'>
+      <div className='c-cb'>
+        <input id={id} type='checkbox' defaultChecked={completed} />
+        <label className='todo-label' htmlFor='todo-0'>
+          {name}
+        </label>
+      </div>
+      <div className='btn-group'>
+        <button type='button' className='btn'>
+          Edit <span className='visually-hidden'>Eat</span>
+        </button>
+        <button type='button' className='btn btn__danger'>
+          Delete <span className='visually-hidden'>Eat</span>
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default Todo
